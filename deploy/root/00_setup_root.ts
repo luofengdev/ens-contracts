9cc7ee26aab9259435a9ce57f476b241a7414dbd
luofengdev <132212510+luofengdev@users.noreply.github.com>
2023-10-30 12:37:48 +0600

    Successfully deployed on goerli

--- deploy/root/00_setup_root.ts
+++ deploy/root/00_setup_root.ts
@@ -18,6 +18,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const registry = await ethers.getContract('ENSRegistry')
   const root = await ethers.getContract('Root')
 
+  console.log(root.address)
+
   const tx1 = await registry.setOwner(ZERO_HASH, root.address)
   console.log(
     `Setting owner of root node to root contract (tx: ${tx1.hash})...`,
@@ -25,6 +27,9 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   await tx1.wait()
 
   const rootOwner = await root.owner()
+
+  console.log(`rootOwner: ${rootOwner}`)
+  console.log(`owner: ${owner}`)
 
   switch (rootOwner) {
     case deployer:
